739e09d8fb83be93e983fc45d304e6ee5f674b53
muhammad galuh gumelar <mggart39@gmail.com>
2026-02-12 13:26:25 +0700

    revisi

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Modern Portfolio",
